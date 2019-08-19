4919bafc26f07b012295628add09b70c255f39f0
Luís Ferreira <contact@lsferreira.net>
2019-08-19 09:06:46 +0100

    ci: add circleci config file and script for publishing
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ scripts/publish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p artifacts/
+
+pushd src
+	if [[ $1 == "netcoreapp3.0" ]]; then
+		dotnet publish -c Release -f $1 -r $2 /p:PublishSingleFile=true /p:UsePreviewVersion=true
+		if [[ $2 == win-* ]]; then
+			BIN_EXT=".exe"
+		else
+			BIN_EXT=""
+		fi
+		mv -f "bin/Release/$1/$2/publish/Codacy.CSharpCoverage$BIN_EXT" "../artifacts/Codacy.CSharpCoverage_$2$BIN_EXT"
+	else
+		dotnet publish -c Release -f $1 -r $2
+		pushd bin/Release/$1/$2/publish
+			BIN_OUTPUT="csharp-codacy-coverage_$2"
+			if [[ $2 == linux-* ]] || [[ $2 == osx-* ]] ; then
+				BIN_OUTPUT+=".tar.gz"
+				tar -czvf $BIN_OUTPUT *
+			else
+				BIN_OUTPUT+=".zip"
+				zip -r $BIN_OUTPUT *
+			fi
+			mv -f $BIN_OUTPUT ../../../../../../artifacts/
+		popd
+	fi
+popd
